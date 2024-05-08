6f3e24f6979f9dbc250a62c53baf3ba0651cb332
Luis Angel Naranjo Giraldo <luis.naranjo@GRUPOMAS.COM>
2024-05-08 10:05:12 -0500

    desplegando app

---
+++ app-old.js
@@ -0,0 +1,23 @@
+
+
+import http from "http";
+
+http.createServer( (req, res)  => {+
+
+    console.log(req)
+
+    // res.writeHead(200, { 'Content-Type': 'application/json' })
+    // res.setHeader('Content-Disposition', 'attachment; filename=lista.csv')
+    // res.writeHead(200, { 'Content-Type': 'application/csv' })
+
+    const persona = {
+        id: 1,
+        nombre: 'Fernando'
+    }
+
+    res.write('Hola Mundo' );
+    res.end();
+
+}).listen( 8080 );
+
+console.log('Escuchando en el perto 80')
--- app.js
+++ app.js
@@ -1,1 +1,50 @@
-console.log('Hola Mundo')
+import express from "express";
+import hbs from "hbs";
+import path from 'path';
+import "dotenv/config";
+
+
+
+const __dirname = path.resolve();
+const app = express();
+const port = process.env.PORT;
+
+// Handlebars
+app.set('view engine', 'hbs')
+hbs.registerPartials(__dirname + '/views/partials');
+
+// Servir contenido estático
+app.use( express.static('public') )
+
+
+app.get('/', (req, res) => { 
+    res.render('home', {
+        nombre: 'Luis Angel',
+        titulo: 'Curso de Node'
+    })
+})
+
+
+app.get('/generic', (req, res) => { 
+    res.render('generic', {
+        nombre: 'Luis Angel',
+        titulo: 'Curso de Node'
+    })
+})
+
+app.get('/elements', (req, res) => { 
+    res.render('elements', {
+        nombre: 'Luis Angel',
+        titulo: 'Curso de Node'
+    })
+})
+
+app.get('*', (req, res) => { 
+    res.sendFile( __dirname + '/public/404.html')
+})
+
+
+
+app.listen( port, () => {
+    console.log(`Example app listening at http://localhost:${ port }`)
+})
